dc5c34ae4bcfcd7c638d073cc1741e4449c5e56d
Leonardo <leo.guzman.cruz04@gmail.com>
2025-09-19 15:17:33 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+export interface Tarea {
+  id: number;
+  titulo: string;
+  completada: boolean;
+}
+
+class GestorTareas {
+  private tareas: Tarea[] = [];
+
+  agregarTarea(tarea: Tarea) {
+    this.tareas.push(tarea);
+  }
+
+  listarTareas(): Tarea[] {
+    return this.tareas;
+  }
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'clase-angular';
+  lista: Tarea[] = [];
+  gestor = new GestorTareas();
+
+  ngOnInit(): void {
+    // Agregamos algunas tareas de prueba
+    this.gestor.agregarTarea({
+      id: 1,
+      titulo: 'Estudiar Angular',
+      completada: false,
+    });
+    this.gestor.agregarTarea({
+      id: 2,
+      titulo: 'Practicar TypeScript',
+      completada: true,
+    });
+
+    // Cargamos la lista
+    this.lista = this.gestor.listarTareas();
+  }
 }
